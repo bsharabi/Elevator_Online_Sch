14e700e19639857d7588e62371c4ccb0d1679ef0
sagir567 <sagir567@gmail.com>
2021-11-01 02:36:11 +0200

    up

--- src/ex0/algo/EL_AlgoTest.java
+++ src/ex0/algo/EL_AlgoTest.java
@@ -1,11 +1,8 @@
-
 package ex0.algo;
-
 import ex0.Building;
 import ex0.CallForElevator;
-import ex0.Elevator;
 import ex0.simulator.*;
-import org.w3c.dom.ls.LSOutput;
+
 
 import java.text.MessageFormat;
 
@@ -14,23 +11,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class EL_AlgoTest {
 
     //Building b= new Building_A(int Min_floor,int Max_floor, int number_of elevators, String name;
-    Building b = new Builging_A( 3, 10, 2,"Sagi_is your_boss");
+    Building b = new Builging_A(3, 10, 2, "Sagi_is your_boss");
 
     //  Call_A c1 = new Call_A(int state, int src, int dest);
     CallForElevator c1 = new Call_A(0, 3, 4);// 3--->5
     CallForElevator c2 = new Call_A(0, 3, 4);//3-->4
     CallForElevator c3 = new Call_A(0, 10, 3);//10---3
-    CallForElevator c4 = new Call_A(0,3 , 9);//3---9
+    CallForElevator c4 = new Call_A(0, 3, 9);//3---9
 
 
-    EL_Algo case1= new EL_Algo(b);
-
-
+    EL_Algo case1 = new EL_Algo(b);
 
 
     @org.junit.jupiter.api.Test
     void getBuilding() {
-        assertTrue(b==case1.getBuilding());
+        assertSame(b , case1.getBuilding());
     }
 
     @org.junit.jupiter.api.Test
@@ -41,18 +36,18 @@ class EL_AlgoTest {
     @org.junit.jupiter.api.Test
     void allocateAnElevator() {
         System.out.println(b.minFloor());
-        System.out.printf("dest:%d %nsrc:%d  %nstate:%d %n", c1.getDest(),c1.getSrc(), c1.getState());
-        System.out.println(MessageFormat.format("dest:{1} \nsrc:{1}  \nstate:{1} ", c1.getDest(),c1.getSrc(), c1.getState()));
-        int t1 = case1.allocateAnElevator( c1);
+        System.out.printf("dest:%d %nsrc:%d  %nstate:%d %n", c1.getDest(), c1.getSrc(), c1.getState());
+        System.out.println(MessageFormat.format("dest:{1} \nsrc:{1}  \nstate:{1} ", c1.getDest(), c1.getSrc(), c1.getState()));
+        int t1 = case1.allocateAnElevator(c1);
         int t2 = case1.allocateAnElevator(c2);
         int t3 = case1.allocateAnElevator(c3);
         int t4 = case1.allocateAnElevator(c4);
-        System.out.printf("t1 %d \nt2: %d \nt3:%d \nt4:%d",t1,t2,t3,t4 );
+        System.out.printf("t1 %d \nt2: %d \nt3:%d \nt4:%d", t1, t2, t3, t4);
 
-        assertEquals(t1,t2);
-        assertEquals(t2,t3);
-        assertEquals(t1,t3);
-        assertNotEquals(t3,t4);
+        assertEquals(t1, t2);
+        assertEquals(t2, t3);
+        assertEquals(t1, t3);
+        assertNotEquals(t3, t4);
     }
 
     @org.junit.jupiter.api.Test
